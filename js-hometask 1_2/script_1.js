24c75dcdafe6de4782a7784e4d0302b0dfa68320
oleh moskovkin <hellfirebringer666@gmail.com>
2017-01-14 19:51:29 +0200

    Hometask 1_2 finished

--- js-hometask 1_2/script_1.js
+++ js-hometask 1_2/script_1.js
@@ -1,19 +1,31 @@
-function pow (num,power) {
-	var result =num;
+var num = prompt('Enter the num', '');
+var power = prompt('Enter the power', '');
 
-	for (var i = 1; i < power; ++i) {
-		result *= num;
-	};
-	for (var i = - 1; i > power; i--) {
-		result =(1/(result*num));
-	};
-	if (num==0) {result==null} else{ if (power==0) {result=1};};
+//почему-то не срабатывает, проверка на ввод только цифр
+// while (num === NaN ) {
+// 	var num = prompt('Enter the num', '');
+// }
+// while (power === NaN) {
+// var power = prompt('Enter the power', '');
+// }
+function pow (num,power) {
+	var result = num;
+if (power >= 1){
+	for (var i = 1; i < power ;i++) 
+	{result *= num;}
+	}
+
+else if (power < 0){
+
+ for (var i =0 ; i < (power*(-1)); i++) 
  
+ {result *= num}
+result=(1/result*num);
+}
+
+else if (power==0) {result=1}
+ else {result==null}
 
 	return result;
-
 }
-var num = prompt('Enter the number', '');
-var power = prompt('Enter the power-number', '');
-
 console.log(pow(num,power));
--- js-hometask 1_2/script_2.js
+++ js-hometask 1_2/script_2.js
@@ -1,24 +1,28 @@
 var arr = [];
-
-for (var i = 0; i < 5; i++) {
+var len = 5; 
+for (var i = 0; i < len ; i++) {
+	
+// недопущение ввода пустой строки
+	do{
 	var userName = prompt('Введите имя', '');
-	if (userName == '') {userName = prompt('Введите имя', '')};
-	if (userName == null) break;
-	arr.push(userName);
+}
+	while (userName === ''); 
+	
+	if (userName === null) break;
+	arr.push(userName);	
 }
 alert(arr);
-
 var compareName = prompt('Введите имя для сравнения', '');
-
-for (var i = 0;i < 1 ;i++) {
-
-
-	if (compareName == null) break;
-
-	if (compareName == arr[0]) {alert(arr[0]+', вы успешно вошли')} 
-		else if (compareName == arr[1]) {alert(arr[1]+', вы успешно вошли');}
-	else if (compareName == arr[2]) {alert(arr[2]+', вы успешно вошли')}
-	else if (compareName == arr[3]) {alert(arr[3]+', вы успешно вошли');}
-	else if (compareName == arr[4]) {alert(arr[4]+', вы успешно вошли');}
-	else {alert('Ошибка')}
+function searchName(compareName , arr) {
+ var text = 'Совпадений не найдено!';
+var len = arr.length;
+ for (var i = 0; i < len; i++) {
+ if (arr[i] === compareName) {
+ text = compareName + ', вы успешно вошли';
+break; 
 }
+
+
+ } return alert( text );
+ }
+ searchName(compareName , arr);
