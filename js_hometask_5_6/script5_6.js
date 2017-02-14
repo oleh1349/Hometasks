a3e7b5018122c367b25694d9d3718463d01e3c73
oleh1349 <hellfirebringer666@gmail.com>
2017-02-14 12:44:01 +0200

    Add files via upload
    Style5__6 added (accidently deleted) & script5_6 modified

--- js_hometask_5_6/script5_6.js
+++ js_hometask_5_6/script5_6.js
@@ -1,55 +1,68 @@
 var active = false;
-var act = false;
-function startTimer(){
-	if(active){
+var time  = new Date();
+
 		var timer = document.querySelector('.timeVisual').innerHTML;
 		var arr = timer.split(":");
+		
 		var hour = arr[0];
 		var min = arr[1];
 		var sec = arr[2];
 		
-			if (sec == 59) {
-				if (min == 59){
-					hour++;
-					min = 0;
-					if (hour < 10) hour = "0" + hour;
-				}else{min++;} 
-
-			if (min < 10) min = "0" + min;
-			sec = 0;}
-		else{sec++;
-		if (sec < 10 ) sec = "0" + sec;
-		}
-		setTimeout(startTimer, 1000);
-
-
-		document.querySelector('.timeVisual').innerHTML = hour + ":" + min + ":" + sec;
-	}
 		
-
-}
-		
-function msecStartTimer(){
-	if (act) {
 		var msec_timer = document.querySelector('.msecTimeVisual').innerHTML;
 		var array = msec_timer.split(":");	
 		var msec = array[0];
-		
-		if (msec == 999) msec = 0;
-			else{msec++};
+
+function startTimer(){
+	if(active){
 			
-		setTimeout(msecStartTimer, 1);
+		var time2 = Date.now();
+		var time3 = time2 - time;
+
+		// msec = time3.getMilliseconds();
+		// sec = time3.getSeconds();
+		// min = time3.getMinutes();
+		// hour = time3.getHours();
+
+		msec = time3%1000;
+
+		sec=Math.floor((time3 / 1000) % 60);
+		if (sec < 10 ) sec = "0" + sec;
+
+		min=Math.floor((time3 / 60000) % 60);
+		if (min < 10) min = "0" + min;
+
+		hour=Math.floor((time3 / 3600000) % 60);
+		if (hour < 10) hour = "0" + hour;
+		
+		
+		}
+		setTimeout(startTimer, 1);
+
 		 document.querySelector('.msecTimeVisual').innerHTML = msec;	
-    }
-}
+		document.querySelector('.timeVisual').innerHTML = hour + ":" + min + ":" + sec;
+	}
+
+// function msecStartTimer(){
+// 	if (act) {
+// 		var msec_timer = document.querySelector('.msecTimeVisual').innerHTML;
+// 		var array = msec_timer.split(":");	
+// 		var msec = array[0];
+		
+// 		if (msec == 999) msec = 0;
+// 			else{msec++};
+			
+// 		setTimeout(msecStartTimer, 1);
+// 		 document.querySelector('.msecTimeVisual').innerHTML = msec;	
+//     }
+// }
 	
 
 function pauseContTimer(){
-	if (active == false && act == false){
+	if (active == false ){
 		active = true;
-		act = true;
 		startTimer();
-		msecStartTimer();
+
 		document.getElementById('startButt').innerHTML = "PAUSE";
 
 		document.getElementById('startButt').style.backgroundColor = "#2f92ea";
@@ -58,7 +71,7 @@ function pauseContTimer(){
 
 	else{
 		active = false;
-		 act = false;
+
 		document.getElementById('startButt').innerHTML = "CONTINUE";
 		document.getElementById('startButt').style.backgroundColor = "#aca72a";
 	}
@@ -67,7 +80,14 @@ function pauseContTimer(){
 document.getElementById('startButt').addEventListener('click', pauseContTimer);
 
 function clearTimer(){
-	document.querySelector('.timeVisual').innerHTML = "00" + ":" + "00" + ":" + "00";
+
+	msec = '000';
+		sec = '00';
+		min = '00';
+		hour = '00';
+
+		var time4 = new Date();
+		var time3 = time4 - time;
 	document.querySelector('.msecTimeVisual').innerHTML  = "000";
 	document.getElementById('startButt').innerHTML = "START";
 	document.getElementById('startButt').style.backgroundColor = "green";
